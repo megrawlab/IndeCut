06e6d051bbf1a862312b13ef4b1fab8a88f27120
Mitra Ansariola <mitra.ansari@gmail.com>
2017-06-05 12:28:49 -0700

    plot_cutnorms_update

--- src/plot_cutnorms_for_algorithms.R
+++ src/plot_cutnorms_for_algorithms.R
@@ -2,11 +2,11 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 1) {
-    cat("Usage ./merge_two_sets.R [cutnorm_infile.txt]\n")
+    cat("Usage ./plot_cutnorms_for_algorithms.R [cutnorm_infile.txt]\n")
     quit(save="no", status=1)
 }
 
-cutnorm_infile <- "Downloads/human_net_cutnorms.txt"
+cutnorm_infile <- "Downloads/even_graphs_cutnorms.txt"
 
 library(ggplot2)
 path <- "."
@@ -30,16 +30,42 @@ Algorithm = factor(data$algorithm)
 
 g1 <- ggplot(data, aes(y = Algorithm)) + labs(x = "cutnorm", y = "algorithm")  + 
   geom_segment(aes(x = data$lower_bound, y = Algorithm, xend = data$upper_bound, yend = Algorithm, color = Algorithm), size = 1)  +
-  geom_point(aes(x = data$lower_bound, color = Algorithm), size = 4, shape = 'I') +
-  geom_point(aes(x = data$upper_bound, color = Algorithm), size = 4, shape = 'I') + 
+  geom_point(aes(x = data$lower_bound, color = Algorithm), size = 5, shape = 'I') +
+  geom_point(aes(x = data$upper_bound, color = Algorithm), size = 5, shape = 'I') + 
   facet_wrap(~graph, ncol = 1) + scale_x_continuous(breaks=seq(0,0.9, 0.08)) +
-  theme_bw() + theme(axis.text = element_text(size=8)) +
-  scale_color_manual(values=c("firebrick1", "chartreuse3", "cyan3", "darkorchid1"))
+  theme_bw() +
+  scale_color_manual(values=c("firebrick1", "chartreuse3", "cyan3", "darkorchid1")) +
+  theme(axis.text = element_text(size=9, color="black",face="bold")) + 
+  theme(legend.text=element_text(size=9,face="bold")) +
+  theme(strip.text = element_text(size=9,face="bold"))
   
 g1
 ggsave(g1, file=paste(cutnorm_infile, "_4alg_plot.jpg", sep = ""))
 
-threealg <- subset(data, !(data$algorithm %in% "CoMoFinder"))
+###### Empty values in ComoFinder related fields
+comoNull <- data
+comoNull[comoNull$algorithm=="CoMoFinder", c("lower_bound", "upper_bound")] <- NA
+comoNull$algorithm <- ifelse(comoNull$algorithm == "CoMoFinder", "*CoMoFinder", algorithms[comoNull$algorithm])
+
+Algorithm = factor(comoNull$algorithm)
+
+g3 <- ggplot(comoNull, aes(y = Algorithm)) + labs(x = "cutnorm", y = "algorithm")  + 
+  geom_segment(aes(x = lower_bound, y = Algorithm, xend = upper_bound, yend = Algorithm, color = Algorithm), size = 1)  +
+  geom_point(aes(x = lower_bound, color = Algorithm), size = 4, shape = 'I') +
+  geom_point(aes(x = upper_bound, color = Algorithm), size = 4, shape = 'I') + 
+  facet_wrap(~graph, ncol = 1) + 
+  #scale_x_continuous(breaks=seq(0,0.9, 0.04)) +
+  theme_bw() +
+  scale_color_manual(values=c("cyan3", "darkorchid1", "chartreuse3","firebrick1")) +
+  theme(axis.text = element_text(size=10, color="black",face="bold")) + 
+  theme(legend.text=element_text(size=10,face="bold")) +
+  theme(strip.text = element_text(size=10,face="bold"))
+g3
+
+ggsave(g3, file=paste(cutnorm_infile, "_comoFNull_plot.jpg", sep = ""), height = 6)
+###########
+
+threealg <- subset(data, !(data$algorithm %in% "*CoMoFinder"))
 Algorithm = factor(threealg$algorithm)
 
 g2 <- ggplot(threealg, aes(y = Algorithm)) + labs(x = "cutnorm", y = "algorithm")  + 
@@ -48,8 +74,11 @@ g2 <- ggplot(threealg, aes(y = Algorithm)) + labs(x = "cutnorm", y = "algorithm"
   geom_point(aes(x = upper_bound, color = Algorithm), size = 4, shape = 'I') + 
   facet_wrap(~graph, ncol = 1) + 
   #scale_x_continuous(breaks=seq(0,0.9, 0.04)) +
-  theme_bw() + theme(axis.text = element_text(size=8)) +
-  scale_color_manual(values=c("firebrick1", "chartreuse3", "darkorchid1"))
+  theme_bw() +
+  scale_color_manual(values=c("firebrick1", "chartreuse3", "darkorchid1")) +
+  theme(axis.text = element_text(size=9, color="black",face="bold")) + 
+  theme(legend.text=element_text(size=9,face="bold")) +
+  theme(strip.text = element_text(size=9,face="bold"))
 g2
 
 ggsave(g2, file=paste(cutnorm_infile, "_3alg_plot.jpg", sep = ""))
